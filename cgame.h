ec253f5d548635dca86fe5415ab46776f586ff23
Ty <60036684+TyGillespie@users.noreply.github.com>
2021-02-15 12:23:08 -0700

    Added initialize function.

--- cgame.h
+++ cgame.h
@@ -12,12 +12,25 @@
 #include<stdlib.h>
 #include <time.h>
 
+int initialized = 0;
+
+int initialize();
 int randInt(int min, int max);
+
+int initialize()
+{
+	if (initialized == 1)
+	{
+		return 0;
+	}
+	srand(time(NULL));
+	intialized = 1;
+	return 1;
+}
 
 int randInt(int min, int max)
 {
 	// Todo: find a better way of generating random numbers.
-	srand(time(NULL));
 	int theNumber = (rand() % (max - min + 1)) + min;
 	return theNumber;
 }
