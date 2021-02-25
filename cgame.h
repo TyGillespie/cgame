b978c55e1550d6c5bd2291055260cd208045e63e
Ty <60036684+TyGillespie@users.noreply.github.com>
2021-02-24 21:42:31 -0700

    Added cgame_ prefix back.

--- cgame.h
+++ cgame.h
@@ -14,13 +14,13 @@
 
 int initialized = 0;
 
-int initialize();
-int randInt(int min, int max);
-double areaOfSquare(double sideLength);
-double areaOfRectangle(double length, double width);
-double areaOfTriangle(double length, double width);
+int cgame_initialize();
+int cgame_randInt(int min, int max);
+double cgame_areaOfSquare(double sideLength);
+double cgame_areaOfRectangle(double length, double width);
+double cgame_areaOfTriangle(double length, double width);
 
-int initialize()
+int cgame_initialize()
 {
 	if (initialized == 1)
 	{
@@ -31,25 +31,25 @@ int initialize()
 	return 1;
 }
 
-int randInt(int min, int max)
+int cgame_randInt(int min, int max)
 {
 	int theNumber = (rand() % (max - min + 1)) + min;
 	return theNumber;
 }
 
-double areaOfSquare(double sideLength)
+double cgame_areaOfSquare(double sideLength)
 {
 	// All sides of a square will be the same, so just multiply the side length times itself.
 	return sideLength * sideLength;
 }
 
-double areaOfRectangle(double length, double width)
+double cgame_areaOfRectangle(double length, double width)
 {
 	// Simply use the area formula (area = length * width) here.
 	return length * width;
 }
 
-double areaOfTriangle(double length, double width)
+double cgame_areaOfTriangle(double length, double width)
 {
 	// We simply use the area formula (length * width) here, but divide it by two because the shape we're dealing with is a triangle.
 	return (length * width) / 2;
--- test.c
+++ test.c
@@ -11,14 +11,14 @@
 
 int main()
 {
-	initialize();
+	cgame_initialize();
 	for (int i = 0; i < 10; i++)
 	{
-		int rng = randInt(1, 10);
+		int rng = cgame_randInt(1, 10);
 		printf("Random number = %d\n", rng);
 	}
-	printf("The area of a 3 by 3 square is: %f", areaOfSquare(3));
-	printf("The area of a 3 by 5 rectangle is: %f", areaOfRectangle(3, 5));
-	printf("The area of a 4 by 4 triangle is: %f", areaOfTriangle(4, 4));
+	printf("The area of a 3 by 3 square is: %f", cgame_areaOfSquare(3));
+	printf("The area of a 3 by 5 rectangle is: %f", cgame_areaOfRectangle(3, 5));
+	printf("The area of a 4 by 4 triangle is: %f", cgame_areaOfTriangle(4, 4));
 	return 0;
 }
